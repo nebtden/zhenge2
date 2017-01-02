6cd02651f19da5c70bf44ef685523436616f08a4
nebtden <simon.zhang@kilimall.com>
2017-01-02 21:25:45 +0800

    1231   备份

--- test3.php
+++ test3.php
@@ -12,6 +12,7 @@ $order_info['store_name'] = 'aaaaaa';
 $order_info['order_sn'] = 555555+time();
 $order_info['paid_amount'] = 0.01;
 $_SESSION['store_id'] = 1;
+$_SESSION['member_id'] = 1;
 $_SESSION['open_id'] ='o0qLLwNFiueQN-UfYWL0Y7H2xIT8';
 $_SESSION['aaa'] =$order_info;
 var_dump($_SESSION['order_info']);
